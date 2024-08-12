29e46a255cf9de7ed6f5a6c9185ea2394145cbe8
Asif <ak3226519@gmail>
2024-08-12 11:34:58 +0530

    nest js pipes and validation using class validator

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,12 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { HttpExceptionFilter } from './appData/exception/http-exception/http-exception.filter';
 import { AllExceptionFilter } from './appData/exception/all-exception/all-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalFilters(new HttpExceptionFilter())
-
+// app.useGlobalPipes(new ValidationPipe({transform:true}))
   // const httpAdapter = app.get(HttpAdapterHost)
   // app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
   await app.listen(3000);
